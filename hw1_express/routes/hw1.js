f9fbb2fae5e53836e12aa42d0557add878e1fcd3
Po-Yu, Tseng <erictpoyu@gmail.com>
2017-06-06 13:58:26 -0400

    hw1 express

---
+++ hw1_express/routes/hw1.js
@@ -0,0 +1,28 @@
+/**
+ * Created by cengboyu on 2017/6/4.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.send('hi, this is hw1');
+});
+
+
+
+router.get('/:name', function (req, res, next) {
+    let theName = req.params.name
+    res.send(JSON.stringify({"string":theName,"length":theName.length}))
+//    next()
+});
+
+
+router.post('/:name', function (req, res, next) {
+    let theName = req.params.name
+    res.send(JSON.stringify({"string":theName,"length":theName.length}))
+});
+
+module.exports = router;
